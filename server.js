32212e01bb6998458438a0f8aab03b65a5b4eb0f
Madhushika <madkalban@gmail.com>
2022-06-15 20:14:06 +0530

    many changes

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 require ("dotenv").config();
 const express =require("express");
 const mongoose = require("mongoose");
-//import route
+const cookieParser = require("cookie-parser");
+
+//import routes
 const authRoute=require("./routes/auth");
+const toDosRoute=require("./routes/todos");
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cookieParser());
 
 app.get("/api", (req,res) => {
     res.send("To Do List");
 });
 
-// app.post("/name", (req,res) =>{
-//     if(req.body.name){
-//         return res.json({name: req.body.name});
-//     }
-//     else{
-//         return res.status(400).json({error:"No name provided"});
-//     }
-// });
-
 app.use("/api/auth", authRoute);
+app.use("/api/todos", toDosRoute);
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("Connected to database");
