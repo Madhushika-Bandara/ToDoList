3e37656516c736aee36e5165b857763701c3ee4c
Madhushika <madkalban@gmail.com>
2022-06-15 13:21:31 +0530

    API added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require ("dotenv").config();
 const express =require("express");
 const mongoose = require("mongoose");
+//import route
+const authRoute=require("./routes/auth");
 
 const app = express();
 app.use(express.json());
@@ -10,14 +12,16 @@ app.get("/", (req,res) => {
     res.send("To Do List");
 });
 
-app.post("/name", (req,res) =>{
-    if(req.body.name){
-        return res.json({name: req.body.name});
-    }
-    else{
-        return res.status(400).json({error:"No name provided"});
-    }
-});
+// app.post("/name", (req,res) =>{
+//     if(req.body.name){
+//         return res.json({name: req.body.name});
+//     }
+//     else{
+//         return res.status(400).json({error:"No name provided"});
+//     }
+// });
+
+app.use("/api/auth", authRoute);
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("Connected to database");
