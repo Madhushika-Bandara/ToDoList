8d9c1c7532a3b8731092d395a167fef4f0d5982d
Madhushika <madkalban@gmail.com>
2022-06-15 13:08:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,5 +25,7 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
     app.listen(process.env.PORT, ()=>{
         console.log(`Server running on port ${process.env.PORT}`);
     });
+}).catch((error)=>{
+    console.log(error);
 });
 
