1b74b07dd852fac89e9862f01065ff54e5f001ef
Madhushika <madkalban@gmail.com>
2022-06-16 14:08:27 +0530

    done

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,20 +1,41 @@
 import React from "react";
 import { useGlobalContext } from "../context/GlobalContext";
 import { useNavigate } from "react-router-dom";
+import ToDoCard from "./ToDoCard";
+import NewToDo from "./NewToDo";
 
 const Dashboard = () => {
 
-    const {user} = useGlobalContext();
+    const {user, completeToDos, incompleteToDos} = useGlobalContext();
     const navigate = useNavigate();
 
     React.useEffect(() =>{
         if(!user){
             navigate("/");
         }
-    },[user, navigate])
+    },[user, navigate]);
+
     return ( 
-        <h1>Dashboard</h1>
+        <div className="dashboard">
+            <NewToDo/>
+            
+            <div className="todos">
+                {incompleteToDos.map(toDo =>(
+                   <ToDoCard toDo={toDo} key={toDo._id}/>
+                ))}
+            </div>
+
+            {completeToDos.length > 0 && ( 
+                <div className="todos">
+                    <h2 className="todos__title">Complete ToDo s</h2>
+                    {completeToDos.map((toDo) => (
+                        <ToDoCard toDo={toDo} key={toDo._id}/>
+                        ))}
+            </div> 
+            )}
+        </div>
+        
      );
-}
+};
  
 export default Dashboard;
